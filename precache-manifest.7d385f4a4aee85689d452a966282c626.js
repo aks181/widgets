ef8d6cf254721a5e92eb336a86df058002c3a75a
Akshay <akshaysinghal181@gmail.com>
2020-09-03 21:54:55 +0530

    Updates

---
+++ precache-manifest.7d385f4a4aee85689d452a966282c626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb576ca099e2b76f18ca9ee4b3b178de",
+    "url": "/widgets/index.html"
+  },
+  {
+    "revision": "bf0a8ea65c7add0c225d",
+    "url": "/widgets/static/js/2.722102a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/widgets/static/js/2.722102a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14097f50c5dac8ca656",
+    "url": "/widgets/static/js/main.d93edfe7.chunk.js"
+  },
+  {
+    "revision": "a3b7248df9ec66a8e8f5",
+    "url": "/widgets/static/js/runtime-main.4d12c3f6.js"
+  }
+]);
